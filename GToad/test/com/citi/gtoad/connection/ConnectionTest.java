4d6f5194636e8705cdef134d169467b0b9ae11ab
xliangwu <xliangwu@gmail.com>
2012-08-30 10:50:22 +0800

    add a junit test
    Signed-off-by: xliangwu <xliangwu@gmail.com>

--- GToad/test/com/citi/gtoad/connection/ConnectionTest.java
+++ GToad/test/com/citi/gtoad/connection/ConnectionTest.java
@@ -1,14 +1,23 @@
 package com.citi.gtoad.connection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import junit.framework.Assert;
 
 import org.junit.Test;
+
+import com.citi.gtoad.domain.EndPoint;
 
 public class ConnectionTest {
 
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+
+	@Test
+	public void connectionTest() {
+		EndPoint endPoint = null;
+		Assert.assertTrue(endPoint == null);
 	}
 
 }
